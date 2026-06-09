bb1fa447c683ed43c949a68e96a2bf020582ffcb
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2026-06-09 11:28:05 -0300

    feat: enhance styling and theming with new CSS variables and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div>
       <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <Button>Clique aqui</Button>
     </div>
   );
 }
