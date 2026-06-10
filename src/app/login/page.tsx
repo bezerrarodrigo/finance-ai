df21fa49cc5e2606530a8be6ec8135ec202b53e3
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2026-06-10 10:10:11 -0300

    feat: update layout and styling of login page for improved user experience

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,33 +3,31 @@ import Image from "next/image";
 
 const LoginPage = () => {
   return (
-    <div className="grid min-h-screen grid-cols-2">
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex max-w-md flex-col">
+    <div className="grid h-full grid-cols-2">
+      <div className="mx-auto flex h-full max-w-xl flex-col justify-center p-8">
+        <Image
+          className="mb-6"
+          src="/financeLogo.svg"
+          alt="Login Image"
+          width={173}
+          height={39}
+        />
+        <h1 className="mb-3 text-4xl font-bold text-white">Bem-vindo!</h1>
+        <p className="text-muted-foreground font-light">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <Button variant="outline" className="mt-6">
           <Image
-            className="mb-6"
-            src="/financeLogo.svg"
-            alt="Login Image"
-            width={160}
-            height={80}
+            src="/google.svg"
+            alt="Google Icon"
+            width={20}
+            height={20}
+            className="mr-2"
           />
-          <h1 className="mb-3 text-2xl font-bold text-white">Bem-vindo!</h1>
-          <p className="max-w-sm leading-relaxed font-light text-white">
-            A Finance AI é uma plataforma de gestão financeira que utiliza IA
-            para monitorar suas movimentações, e oferecer insights
-            personalizados, facilitando o controle do seu orçamento.
-          </p>
-          <Button variant="outline" className="mt-6">
-            <Image
-              src="/google.svg"
-              alt="Google Icon"
-              width={20}
-              height={20}
-              className="mr-2"
-            />
-            Entrar com Google
-          </Button>
-        </div>
+          Entrar com Google
+        </Button>
       </div>
       <div className="relative h-full w-full">
         <Image
