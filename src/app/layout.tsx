7e12f963855fbdc01115a5c9137dbc170f72d54a
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2026-06-15 10:16:15 -0300

    feat: replace Geist fonts with Mulish for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/ui/themes";
+import type { Metadata } from "next";
+import { Mulish } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const mulish = Mulish({ subsets: ["latin-ext"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Finance A.I",
@@ -30,14 +20,7 @@ export default function RootLayout({
   return (
     <html
       lang="pt-BR"
-      className={cn(
-        "h-full",
-        "antialiased",
-        geistSans.variable,
-        geistMono.variable,
-        "font-sans",
-        inter.variable,
-      )}
+      className={cn("h-full", "antialiased", mulish.variable, "font-sans")}
     >
       <body className="dark flex min-h-full flex-col">
         <ClerkProvider
